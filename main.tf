a948f331aa302e76827199da92bf10a1c6dc1e04
tato <tatoluffy@gmail.com>
2022-02-16 15:22:25 -0500

    Task 1 - Define a new resource s3 bucket with name tf-dbl-20220216

--- main.tf
+++ main.tf
@@ -1,6 +1,10 @@
-# Configuration with aws
+# Configuration AWS provider
 provider "aws" {
     profile = "default"
     region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "tf_s3" {
+    bucket = "tf-dbl-20220216"
+    acl    = "private"
+}
